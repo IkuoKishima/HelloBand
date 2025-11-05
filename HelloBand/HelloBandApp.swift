5ecd6ffe67e5f1f8f03bd260524d733e29f632c7
Kishima Ikuo <kisi-man@users.noreply.github.com>
2025-11-05 21:41:02 +0900

    chore: bootstrap HelloBand (Xcode SwiftUI template)

---
+++ HelloBand/HelloBandApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloBandApp.swift
+//  HelloBand
+//
+//  Created by 木嶋育朗 on 2025/11/05.
+//
+
+import SwiftUI
+
+@main
+struct HelloBandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloBandTests/HelloBandTests.swift
@@ -0,0 +1,16 @@
+//
+//  HelloBandTests.swift
+//  HelloBandTests
+//
+//  Created by 木嶋育朗 on 2025/11/05.
+//
+
+import Testing
+
+struct HelloBandTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
